c7deae6c2b123fad20c9c3ae2af58233bdc9ea15
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-02 14:15:44 +0200

    Cap ID at 124

--- pages/api/fact.js
+++ pages/api/fact.js
@@ -1,7 +1,7 @@
 export default (_req, result) => {
   fetch("https://easterfunction.azurewebsites.net/api/eastertrigger").then(res => res.text()).then(res => {
     const message = res;
-    const numberID = Math.floor((message.length * 2) / (message.split(" ").length / 16));
+    const numberID = (Math.floor((message.length * 2) / (message.split(" ").length / 16))) % 125;
     result.status(200).json({ result: message, number: numberID });
   });
 };
